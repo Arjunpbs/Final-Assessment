bd96f6ca9263f5a6aeb9cb715241d01c84a4696e
arjun p <arjunkannanpbs@gmail.com>
2024-09-01 16:39:25 +0530

    Update connection.js

--- MonsoonExitBatch2/MonsoonExitBatch2/Backend/connection.js
+++ MonsoonExitBatch2/MonsoonExitBatch2/Backend/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //Write missing code 
-mongoose.connect("mongodb+srv://adhilajamalajk:adhilamongo@cluster0.gd9ihr1.mongodb.net/Final?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://arjunpbs:Arjunpbskl19d5569@cluster0.w10l0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("Connected to DB");
   })
